f51db617aa5f30b1c79e2f5063918d60969a2c5f
Sébastien Verbois <sebastien.verbois@imio.be>
2025-02-20 15:09:44 +0100

    refactor z3c.table example

--- src/collective/concepts/tables/contents.py
+++ src/collective/concepts/tables/contents.py
@@ -3,23 +3,26 @@ from plone.dexterity.interfaces import IDexterityContent
 from Products.Five import BrowserView
 from z3c.table import column
 from z3c.table.table import Table
-from zope.component import adapter
+from zope.interface import Interface
 from zope.interface import implementer
 
-from .interfaces import IContentsTable
 
+class ContentsTableView(BrowserView):
 
-class GenericView(BrowserView):
-
+    @property
     def table(self):
-        generic_table = ContentTable(self, self.request)
+        generic_table = ContentsTable(self, self.request)
         generic_table.__parent__ = self
         generic_table.update()
         return generic_table
 
 
+class IContentsTable(Interface):
+    """Marker interface for collective.concepts tables"""
+
+
 @implementer(IContentsTable)
-class ContentTable(Table):
+class ContentsTable(Table):
     cssClasses = {
         "table": "table table-bordered",
     }
@@ -36,3 +39,36 @@ class ContentsTableValues(object):
     def values(self):
         values = [item.getObject() for item in api.content.find(object_provides=IDexterityContent)]
         return values
+
+
+class TitleColumn(column.LinkColumn):
+    header = "Titre"
+    weight = 10
+
+    def getLinkURL(self, item):
+        return item.absolute_url()
+
+    def getLinkContent(self, item):
+        return item.title
+
+    # TODO: créer un adapteur pour la valeur du titre
+
+
+class StateColumn(column.Column):
+    header = "Etat"
+    weight = 100
+
+    def renderCell(self, item):
+        state_title = ""
+        try:
+            state = api.content.get_state(obj=item)
+            wtool = api.portal.get_tool("portal_workflow")
+            workflows = wtool.getWorkflowsFor(item)
+            if workflows:
+                for w in workflows:
+                    if state in w.states:
+                        state_title = w.states[state].title or state
+                        break
+        except:
+            pass
+        return state_title
--- src/collective/concepts/tables/interfaces.py
+++
@@ -1,5 +0,0 @@
-from zope.interface import Interface
-
-
-class IContentsTable(Interface):
-    """Marker interface for collective.concepts tables"""
