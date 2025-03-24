c27c3b300eeac18e091ef7dfd5f80df3b6748836
Sébastien Verbois <sebastien.verbois@imio.be>
2025-03-24 09:12:18 +0100

    Complete books listing view

--- src/collective/concepts/views/books.py
+++ src/collective/concepts/views/books.py
@@ -13,7 +13,7 @@ class BooksView(BrowserView):
             authors = [r.to_object for r in api.relation.get(source=b, relationship="authors")]
             authors_infos = [
                 {
-                    "fullname": a.title,
+                    "fullname": a.firstname + " " + a.lastname,
                     "url": a.absolute_url(),
                 }
                 for a in authors
@@ -24,6 +24,33 @@ class BooksView(BrowserView):
                     "description": b.description,
                     "url": b.absolute_url(),
                     "authors": authors_infos,
+                }
+            )
+        return infos
+
+    @property
+    def authors(self):
+        brains = api.content.find(portal_type="Author", sort_on="sortable_title")
+        authors = [b.getObject() for b in brains]
+        infos = []
+        for a in authors:
+            books = [
+                r.from_object
+                for r in api.relation.get(target=a, relationship="authors")
+                if r.from_object.portal_type == "Book"
+            ]
+            books_infos = [
+                {
+                    "title": b.title,
+                    "url": b.absolute_url(),
+                }
+                for b in books
+            ]
+            infos.append(
+                {
+                    "title": a.title,
+                    "url": a.absolute_url(),
+                    "books": books_infos,
                 }
             )
         return infos
